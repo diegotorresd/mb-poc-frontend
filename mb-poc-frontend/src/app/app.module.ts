aedb006f2b402403cdb18e70be3bd1c14a1c60cd
Diego Torres <diego.torres@upm.es>
2018-09-02 11:08:14 +0200

    Add material and materialize

--- mb-poc-frontend/src/app/app.module.ts
+++ mb-poc-frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DirectorComponent } from './director/director.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectorComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
